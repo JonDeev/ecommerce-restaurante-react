325ad0cb1b46b1679702af2aaf1026e0485bb5e4
John Pertuz <johnpertuzsism@gmail.com>
2022-03-03 13:40:37 -0500

    Creacion del proyecto ecommerce restauran pizza y creacion del header

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav, NavLink } from "./NavbarElements";
+
+export const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>PIZAAA</NavLink>
+      </Nav>
+    </>
+  );
+};
